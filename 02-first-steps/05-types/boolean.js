137e0bf3121caecd99ac25f475ebe88fd27f2df4
cloud99 <1imepie.c1oud9@gmail.com>
2024-05-23 22:48:36 +0900

    Edit files in 02-first-steps/05-types folder

---
+++ 02-first-steps/05-types/boolean.js
@@ -0,0 +1,7 @@
+let nameFieldChecked = true; // 네, name field가 확인되었습니다(checked).
+let ageFieldChecked = false; // 아니요, age field를 확인하지 않았습니다(not checked)
+
+
+
+let isGreater = 4 > 1;
+alert( isGreater ); // true (비교 결과: "yes")
