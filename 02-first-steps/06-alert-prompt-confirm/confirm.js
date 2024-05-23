809a4073c71c03d247daee923227f40f2727ec55
cloud99 <1imepie.c1oud9@gmail.com>
2024-05-23 23:12:16 +0900

    Edit files in 02-first-steps/06-alert-prompt-confirm folder

---
+++ 02-first-steps/06-alert-prompt-confirm/confirm.js
@@ -0,0 +1,7 @@
+// confirm 함수는 매개변수로 받은 question(질문)과 확인 및 취소 버튼이 있는 모달 창을 보여준다.
+// 사용자가 확인 버튼을 누르면 true, 그 외의 경우는 false를 반환
+result = confirm(question);
+
+
+let isBoss = confirm('당신이 주인인가요?');
+alert(isBoss);  // 확인 버튼을 눌렀다면 true 출력
